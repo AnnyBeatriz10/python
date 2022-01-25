2797d45ac6277fa5063bc93d27d31afcfd1cd7c6
AnnyBeatriz10 <75858073+AnnyBeatriz10@users.noreply.github.com>
2022-01-25 12:03:55 -0300

    media

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-b = int(input("digite a base:"))
-h = int(input("digite a altura:"))
-a = (b*h)//2
-print("a área do triângulo é:",a)
+ndig= 0
+soma = 0
+num = int(input("digite um numero:"))
+while num>(-1):
+   ndig+= 1
+   soma = soma+num
+   num = int(input("digite um numero:"))
+media = soma/ndig
+print("a media é:",media)
+if ndig>0:
+   print("a media é:",soma/ndig)
+else:
+   print("não é possível calcular a média")
+    
