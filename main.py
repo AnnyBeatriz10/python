23473b1b6dfaf2b4ce5b0585338e0356dbb37acd
AnnyBeatriz10 <75858073+AnnyBeatriz10@users.noreply.github.com>
2022-01-25 12:05:25 -0300

    triangulo de pascal

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-b = int(input("digite a base:"))
-h = int(input("digite a altura:"))
-a = (b*h)//2
-print("a área do triângulo é:",a)
+#elementos = (input("Digite a quantidade de elementos:"))
+l = [1]
+print(l)
+for lin in range(2,10+1):
+#for lin in range(2,elementos+1):
+  l2 = []
+  for i in range(0,len(l)-1):
+    l2.append(l[i] + l[i+1])
+  l2 = [1] + l2 + [1]
+  print(l2)
+  l = l2
+  
+  
