bbd1b59572864037cad3512b02a0f32998b36177
AnnyBeatriz10 <75858073+AnnyBeatriz10@users.noreply.github.com>
2022-01-25 12:02:07 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-b = int(input("digite a base:"))
-h = int(input("digite a altura:"))
-a = (b*h)//2
-print("a área do triângulo é:",a)
+palavra = "chinelo"
+palavra2 = "_" * len(palavra)
+tentativas = 0
+digitados = ""
+chance = 0
+while palavra2 != palavra:
+	letra = input("informe uma letra:")
+	digitados +=letra
+	pos = palavra.find(letra)
+	while pos != -1:
+		palavra2 = palavra2[0:pos] + letra + palavra2[pos + 1:]
+		pos = palavra.find(letra, pos + 1)
+		print(palavra2)
+		tentativas += 1
+if palavra2==len(palavra):
+  print("ops!,você tem mais uma chance")
+  if palavra2!=palavra:
+    chance = input("digite a palavra",palavra2)
+
